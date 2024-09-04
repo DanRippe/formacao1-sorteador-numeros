97fba395e114cb10bda90315b8a85188c9d5f64e
Danilo Cardoso <danilo.mecatronica2015@gmail.com>
2024-09-04 18:09:32 -0300

    Inicio de projeto

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let numeroInicial = document.getElementById('de').value;
+    let numeroFinal = document.getElementById('ate').value;
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do número: ${numeroInicial}`);
+    alert(`Até o número: ${numeroFinal}`);
+}
